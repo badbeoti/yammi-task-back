1b25001d127c41d3ce69d20f0c06fdb3aab4cdfd
ChanSolP <badbeoti4251@gmail.com>
2021-12-30 16:40:27 +0900

    feat: 거래 내역 조회 api 추가 
    <description> 
    
    <issue-number> 

--- transfer/urls.py
+++ transfer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import airdrop, send
+from .views import airdrop, send, AllTransactions
 
 urlpatterns = [
     path("airdrop/", airdrop),
-    path("send/", send)
+    path("send/", send),
+    path('transactions/', AllTransactions.as_view(), name='transactions')
 ]
--- transfer/views.py
+++ transfer/views.py
@@ -1,3 +1,6 @@
+from rest_framework.generics import ListAPIView
+from django.db.models import Q
+
 from yammiback.models import MyUser
 
 from rest_framework.decorators import api_view, permission_classes
@@ -5,6 +8,24 @@ from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from .models import Transfer
 from .serializers import TransferSerializer, AirdropTransferSerializer
+
+
+class AllTransactions(ListAPIView):
+    queryset = Transfer.objects.all()
+    serializer_class = TransferSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset().filter(Q(id_sender=request.user.id) | Q(id_receiver=request.user.id))
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(queryset, many=True)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        return Response(serializer.data)
+
 
 
 @api_view(['POST'])
